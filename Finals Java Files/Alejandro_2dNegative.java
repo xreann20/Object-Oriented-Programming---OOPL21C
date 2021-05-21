f16297ddebbdce797d27c4b6731a7bc8e16e83c3
xreann20 <xreann20@gmail.com>
2021-05-21 19:07:55 +0800

    Add files via upload

--- Finals Java Files/Alejandro_2dNegative.java
+++ Finals Java Files/Alejandro_2dNegative.java
@@ -11,11 +11,11 @@ public class Alejandro_2dNegative {
 		 * 2D Array size example is 5x3
 		 */
 		int array_2d[][]= {{10,-20,-3,12,-13},{8,-7,3,76,21},{31,-99,77,66,-1}};
-		
-		checkNegativesOnArray(array_2d);
+		//Call CheckNegativesOnArray
+		System.out.println("The number of negative elements in the 2D Array is " + checkNegativesOnArray(array_2d) + ".");
 	}
 	
-		public static void checkNegativesOnArray(int array_2d[][])
+		public static int checkNegativesOnArray(int array_2d[][])
 		{
 			int neg_num=0;
 			for(int row=0; row<array_2d.length; row++)
@@ -28,11 +28,13 @@ public class Alejandro_2dNegative {
 				{
 					if(array_2d[row][col]<0)
 					{
+						//Add + 1 to every negative number processed
 						neg_num=neg_num+1;
 					}
 				}
 			}
-			System.out.println("The number of negative elements in the 2D Array is " + neg_num + ".");
+			//Then return the value to main
+			return neg_num;
 		}
 	
 }
--- Finals Java Files/Alejandro_SameArray.java
+++ Finals Java Files/Alejandro_SameArray.java
@@ -1,5 +1,5 @@
 /*
-12:29 pm 21/05/2021
+5:40 pm 21/05/2021
 John Russ Alejandro
 */
 
@@ -9,32 +9,31 @@ public class Alejandro_SameArray {
 		/*
 		 * Again, I initialized both arrays A and B
 		 * up to size 10 just a proof of concept that 
-		 * it will just print up to n parameter(5)
+		 * it will just pass size up to n parameter(5)
 		 */
 		int A[]= {10,20,30,40,50,60,70,80,90,100};
-		int B[]= {10,11,30,33,50,66,70,88,90,111};
-		
-		verifyArray(A,B,5);
+		int B[]= {10,20,30,40,50,61,71,81,91,101};
+		/*
+		 * Method Call of verifyArray
+		 */
+		System.out.println(verifyArray(A,B,6));
+
 	}
 	
-	public static void verifyArray(int A[], int B[], int n)
+	public static int verifyArray(int A[], int B[], int n)
 	{
 		for(int i=0; i<n; i++)
-		{
 			/*
-			 * For 0 to n parameter, and if array A[i]=B[i]
-			 * print 1 else print 0, added index for reference
+			 * If A[] is not equal to B[] return 0
+			 * otherwise it is equal hence, return 1
+			 * PS..I could have used a variable to later
+			 * assign as 1 or 0 then return it but for
+			 * shorter code lines I used this method
 			 */
-			if(A[i]==B[i])
-			{
-				System.out.println("index[" + i + "] = 1");
-				
-			}
-			else
-			{
-				System.out.println("index[" + i + "] = 0");
-			}
+		{
+			if(A[i]!=B[i])
+				return 0;
 		}
+		return 1;
 	}
 }
-
